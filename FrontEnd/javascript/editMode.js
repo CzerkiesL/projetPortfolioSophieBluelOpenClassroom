e358a95c83ac9002bc1598c962bfbcbdf77bc741
CzerkiesL <h3llgate@outlook.fr>
2024-06-04 08:47:09 +0200

    verification du formulaire de la modale + affichage de la photo choisie

--- FrontEnd/javascript/editMode.js
+++ FrontEnd/javascript/editMode.js
@@ -1,35 +1,62 @@
-import * as Modal from "./function/modal.js";
-import * as ModalNav from "./function/modalNavigation.js";
+import * as modalNav from "./function/modalNavigation.js";
+import * as modalGallery from "./function/modalGallery.js";
+import * as modalForm from "./function/modalForm.js";
 import { projects } from "./index.js";
 
+////////////////////// charge la partie projet ////////////////////////////////
+modalGallery.generateGalleryContent(projects);
 
-
-Modal.generateGalleryContent(projects);
+modalForm.createCategieOption();
 
 ////////////////////// ouverture de la modale ////////////////////////////////
 const editBtns = document.querySelectorAll("button.edit-btn");
 editBtns.forEach((editBtn) => {
-    editBtn.addEventListener("click", ModalNav.openModal);
+    editBtn.addEventListener("click", () => {
+        modalNav.openModal();
+        modalForm.resetForm();
+    });
 });
 
 ////////////////////// fermeture de la modale ////////////////////////////////
 const closeBtns = document.querySelectorAll("i.fa-xmark");
 closeBtns.forEach((closeBtn) => {
-    closeBtn.addEventListener("click", ModalNav.closeModal);
+    closeBtn.addEventListener("click", modalNav.closeModal);
 });
 
 const modalBg = document.querySelector("div.modal-bg");
-modalBg.addEventListener("click", ModalNav.closeModal);
+modalBg.addEventListener("click", modalNav.closeModal);
 
 ////////////////////// changement de la modale ////////////////////////////////
-const modalBtnValidation = document.querySelectorAll("button.modal-btn");
-modalBtnValidation.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        if (!btn.classList.contains("no-valide")) {
-            ModalNav.toggleModalContent();
-        }
-    });
+const modalBtnValidation = document.getElementById("add-img");
+modalBtnValidation.addEventListener("click", () => {
+    modalNav.toggleModalContent();
 });
 
 const arrowBtn = document.querySelector("i.fa-arrow-left");
-arrowBtn.addEventListener("click", ModalNav.toggleModalContent);
+arrowBtn.addEventListener("click", () => {
+    modalNav.toggleModalContent();
+    modalForm.resetForm();
+});
+
+///////////// verifie si l'image du formulaire est valide ///////////////////
+modalForm.checkImage();
+
+const addForm = document.querySelector("form#modal-form");
+addForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputImg = document.querySelector("input#image");
+    const inputTitle = document.querySelector("input#title");
+    const inputCategory = document.querySelector("select#category");
+
+    const formIsValid = modalForm.checkInput();
+
+    if (formIsValid) {
+        const newProject = {
+            image: inputImg.files[0],
+            title: inputTitle.value,
+            category: inputCategory.value,
+        };
+        console.log(newProject);
+    }
+});
---
+++ FrontEnd/javascript/function/modalForm.js
@@ -0,0 +1,128 @@
+/******************************************  
+ verifie si le ficher selectionner est valide
+ ******************************************/
+export function checkImage() {
+    const inputImg = document.querySelector("input#image");
+    const imgError = document.querySelector("div.err-img");
+
+    inputImg.addEventListener("change", () => {
+        const img = inputImg.files[0];
+
+        if (img.type === "image/jpeg" || img.type === "image/png") {
+            if (img && img.size < 4000000) {
+                imgError.innerHTML = "";
+
+                let reader = new FileReader();
+
+                reader.onload = () => {
+                    const formImgGroup =
+                        document.querySelector("div.form-img-group");
+                    const preveiwImg =
+                        document.querySelector("img.img-preview");
+
+                    formImgGroup.classList.add("hide");
+                    preveiwImg.classList.remove("hide");
+
+                    preveiwImg.src = reader.result;
+
+                    const submitBtn = document.querySelector("button#sub-btn");
+                    submitBtn.removeAttribute("disabled");
+                };
+
+                reader.readAsDataURL(img);
+            } else {
+                createErrorMessage("Fichier trop volumineux", imgError);
+            }
+        } else {
+            createErrorMessage("Mauvais type de fichier", imgError);
+        }
+    });
+}
+
+/******************************************  
+ creer un message d'erreur
+    param 1: le message d'erreur
+    param 2: l'emplacement
+ ******************************************/
+export function createErrorMessage(message, container) {
+    container.innerHTML = "";
+
+    const messageError = document.createElement("p");
+    messageError.style.color = "crimson";
+    messageError.style.fontSize = "12px";
+    messageError.style.margin = "5px 0 0px 5px";
+    messageError.innerText = message;
+    container.appendChild(messageError);
+}
+
+/******************************************  
+ recupere les category pour la selection
+ ******************************************/
+export async function createCategieOption() {
+    const categoryResponse = await fetch(
+        "http://localhost:5678/api/categories"
+    );
+    const categorys = await categoryResponse.json();
+
+    const selectCategory = document.querySelector("select#category");
+
+    categorys.forEach((category) => {
+        const option = document.createElement("option");
+        option.value = category.id;
+        option.innerText = category.name;
+
+        selectCategory.appendChild(option);
+    });
+}
+
+/******************************************  
+ verifie que les entree utilisateur soit valide valide
+ ******************************************/
+export function checkInput() {
+    const inputTitleValue = document.querySelector("input#title").value;
+    const inputCategoryValue = document.querySelector("select#category").value;
+    const titleError = document.querySelector(".err-titre");
+    const categoryError = document.querySelector(".err-category");
+    let erreurCount = 0;
+
+    if (inputTitleValue === "") {
+        createErrorMessage(
+            "veuillez renseigner un titre pour le projet",
+            titleError
+        );
+        erreurCount++;
+    } else {
+        titleError.innerHTML = "";
+    }
+
+    if (inputCategoryValue === "0") {
+        createErrorMessage(
+            "veuillez choisir une categorie pour le projet",
+            categoryError
+        );
+        erreurCount++;
+    } else {
+        categoryError.innerHTML = "";
+    }
+
+    if (erreurCount !== 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/******************************************  
+ reset complet du formulaire
+ ******************************************/
+export function resetForm() {
+    document.querySelector("div.form-img-group").classList.remove("hide");
+    document.querySelector("img.img-preview").classList.add("hide");
+    document.querySelector("button#sub-btn").setAttribute("disabled", true);
+    document.querySelector("input#image").value = "";
+    document.querySelector("input#title").value = "";
+    document.querySelector("select#category").value = "0";
+    document.querySelector("div.err-img").innerHTML = "";
+    document.querySelector(".err-titre").innerHTML = "";
+    document.querySelector(".err-category").innerHTML = "";
+}
